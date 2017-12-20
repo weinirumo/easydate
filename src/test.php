7d8ab1db770f627eea6194fad4628e108066b1af
Weini <weinirumo@126.com>
2017-12-20 17:44:47 +0800

    test commit

---
+++ src/test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2017-12-20 17:42:56
+ * @Last Modified by:   name
+ * @Last Modified time: 2017-12-20 17:43:51
+ */
+namespace weini\easydate;
+
+class test
+{
+    public static function test()
+    {
+        echo "Test success";
+    }
+}
